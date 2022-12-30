0d6e6804757c75e1f6de18845a9b4c682693e3a2
follow <follow@qq.com>
2022-12-30 02:34:36 +0200

    add classification of show products with conditions in home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,67 +31,42 @@ namespace Shops.Controllers
                 ClothesClassifications = await _dbContext.ClothesClassifications.AsNoTracking().ToListAsync(),
                 HumanClassifications = await _dbContext.HumanClass.AsNoTracking().ToListAsync(),
                 AgeStages = await _dbContext.AgeStages.AsNoTracking().ToListAsync(),
+                userProducts = await _dbContext.UserProducts.AsNoTracking().ToListAsync(),
                
             };
-            if (classProduct.HumanClassificationsId == 0 && classProduct.AgeStagesId == 0 && classProduct.ClothesClassificationsId ==0)
+            IQueryable<Product> resultProduct = _dbContext.products;
+            if(classProduct.ClothesClassificationsId >0 && classProduct.AgeStagesId >0 && classProduct.HumanClassificationsId > 0)
             {
-                classificationsOfProducts.Product = await _dbContext.products.Include(p => p.Marka).Include(p => p.ClothesClassification)
-                    .AsSplitQuery().AsNoTracking().ToListAsync();
-                return View(classificationsOfProducts);
+                resultProduct = resultProduct.Where(c => c.ClothesClassificationId == classProduct.ClothesClassificationsId && c.HumanClassId == classProduct.HumanClassificationsId && c.AgeStageId == classProduct.AgeStagesId);
             }
-            if(classProduct.ClothesClassificationsId >0 && classProduct.AgeStagesId >0)
+            if (classProduct.ClothesClassificationsId > 0 && classProduct.HumanClassificationsId > 0 && classProduct.AgeStagesId ==0 )
             {
-                classificationsOfProducts.Product = await _dbContext.products
-                .Where(c => c.ClothesClassificationId == classProduct.ClothesClassificationsId && c.AgeStageId == classProduct.AgeStagesId)
-               .Include(p => p.Marka).Include(p => p.ClothesClassification)
-                   .AsSplitQuery().AsNoTracking().ToListAsync();
-                return View(classificationsOfProducts);
+                resultProduct = resultProduct.Where(c => c.ClothesClassificationId == classProduct.ClothesClassificationsId && c.HumanClassId == classProduct.HumanClassificationsId);
             }
-            if (classProduct.ClothesClassificationsId > 0)
+            if (classProduct.HumanClassificationsId > 0 && classProduct.AgeStagesId > 0 && classProduct.ClothesClassificationsId == 0)
             {
-                classificationsOfProducts.Product = await _dbContext.products
-                .Where(c => c.ClothesClassificationId == classProduct.ClothesClassificationsId )
-               .Include(p => p.Marka).Include(p => p.ClothesClassification)
-                   .AsSplitQuery().AsNoTracking().ToListAsync();
-                return View(classificationsOfProducts);
+                resultProduct = resultProduct.Where(c => c.HumanClassId == classProduct.HumanClassificationsId && c.AgeStageId == classProduct.AgeStagesId);
             }
-          /*  if (classProduct.AgeStagesId > 0)
-            {*/
-                classificationsOfProducts.Product = await _dbContext.products
-                .Where(c => c.AgeStageId == classProduct.AgeStagesId)
-               .Include(p => p.Marka).Include(p => p.ClothesClassification)
-                   .AsSplitQuery().AsNoTracking().ToListAsync();
-                return View(classificationsOfProducts);
-      
+            if (classProduct.ClothesClassificationsId > 0 && classProduct.AgeStagesId > 0 && classProduct.HumanClassificationsId == 0)
+            {
+                resultProduct = resultProduct.Where(c => c.ClothesClassificationId == classProduct.ClothesClassificationsId && c.AgeStageId == classProduct.AgeStagesId);
+            }
+            if (classProduct.ClothesClassificationsId > 0 && classProduct.HumanClassificationsId == 0 && classProduct.AgeStagesId == 0)
+            {
+                resultProduct = resultProduct.Where(c => c.ClothesClassificationId == classProduct.ClothesClassificationsId);
+            }
+            if (classProduct.HumanClassificationsId> 0 && classProduct.ClothesClassificationsId == 0 && classProduct.AgeStagesId == 0)
+            {
+                resultProduct = resultProduct.Where(c => c.HumanClassId == classProduct.HumanClassificationsId);
+            }
+            if (classProduct.AgeStagesId > 0 && classProduct.HumanClassificationsId == 0 && classProduct.ClothesClassificationsId == 0)
+            {
+                resultProduct = resultProduct.Where(c => c.AgeStageId == classProduct.AgeStagesId);
+            }
+            resultProduct = resultProduct.Include(p => p.Marka).Include(p => p.ClothesClassification);
+            classificationsOfProducts.Product = await resultProduct.AsSplitQuery().AsNoTracking().ToListAsync();
+            return View(classificationsOfProducts);
         }
-      /*  [HttpGet]
-        public async Task< IActionResult> ClassProducts(ClassProduct classProduct)
-        {
-          *//*  List<Product> products = new List<Product>();*/
-
-      
-            /*if (classProduct.AgeStagesId == 0 && classProduct.ClothesClassificationsId == 0 && classProduct.HumanClassificationsId == 0)*//*
-                var products =await _dbContext.products
-                      .AsNoTracking().ToListAsync();
-            *//*
-                         if (classProduct.AgeStagesId != 0)
-                         {
-                             products =  _dbContext.products.Where(p => p.AgeStageId == classProduct.AgeStagesId)
-                                  .Include(p => p.Marka).Include(p => p.ClothesClassification).Include(c => c.Cards).
-                               AsSplitQuery().AsNoTracking().ToList();
-                         }*/
-            /*        if (classProduct.ClothesClassificationsId != 0 )*/
-            /*   products = await _dbContext.products.Where(p => p.ClothesClassificationId == classProduct.ClothesClassificationsId)
-                    .Include(p => p.Marka).Include(p => p.ClothesClassification).Include(c => c.Cards).
-                 AsSplitQuery().AsNoTracking().ToListAsync();*/
-
-          /*  var product = new List<Product>();
-            product = products;
-             *//*
-          
-             return Json(products);
-           
-        }*/
         public async Task< IActionResult> Create()
         {
             ViewProducts viewProducts = new ViewProducts { };
